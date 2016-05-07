c45cbb633993a33ede54c5f4dd95e83b5d93d43c
Jesse Peplinski <peplinski.jesse@gmail.com>
2016-05-07 16:08:06 -0400

    add hamburger logo
    I added a hamburger logo that now displays the navbar options on mobile
    devices and smaller viewports. I also had to import Material Icons
    within layout.html.

--- client/navigation/navigation.js
+++ client/navigation/navigation.js
@@ -1,1 +1,11 @@
+// $( document ).ready(function() {
 
+// 	$(".button-collapse").sideNav();
+
+// });
+
+Template.navigation.onRendered(function () {
+  $('.button-collapse').sideNav({
+    closeOnClick: true
+  });
+});
