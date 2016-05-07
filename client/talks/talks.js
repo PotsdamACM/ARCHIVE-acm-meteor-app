2b01fa934f48b969be084e1c6a7d0a6ba6cfb65c
Collin L. Walling <collin.walling@gmail.com>
2016-05-07 19:32:43 -0400

    modal window style and code docs
    modal window now has max height of 100% and
    addeds some documentation to the talks.js
    for future clarity.

--- client/talks/talks.js
+++ client/talks/talks.js
@@ -1,7 +1,15 @@
 Template.talks.events({
+  // functionality for add button. this button makes the modal appear
+  // which has a form for adding fields for a talk.
   'click .modal-trigger': function(e) {
     e.preventDefault();
     $('#modal1').openModal();
+  },
+
+  // additional functionality for the submit button on the form. this
+  // button will also close the modal window when this button is clicked.
+  'click .btn-primary': function(e) {
+    $('#modal1').closeModal();
   }
 });
 
